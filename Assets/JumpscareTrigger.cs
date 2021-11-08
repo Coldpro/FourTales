d6c212bf2c21a52a9b2055565cde2e469fa3edf3
Coldpro <keshav1185@gmail.com>
2021-11-08 14:05:30 +0100

    JumpCam + New House

--- Assets/JumpscareTrigger.cs
+++ Assets/JumpscareTrigger.cs
@@ -7,22 +7,22 @@ public class JumpscareTrigger : MonoBehaviour {
     public AudioSource Scream;
     public GameObject ThePlayer;
     public GameObject JumpCam;
-    public GameObject FlashIng;
+    //public GameObject FlashIng;
 
 
     void OnTriggerEnter() {
         Scream.Play();
         JumpCam.SetActive(true);
         ThePlayer.SetActive(false);
-        FlashIng.SetActive(true);
-        
+        //FlashIng.SetActive(true);
 
+        StartCoroutine(EndJump(2f));
     }
-    IEnumerator EndJunp() {
-        yield return new WaitForSeconds (2.03f);
+    IEnumerator EndJump(float wait) {
+        yield return new WaitForSeconds (wait);
         ThePlayer.SetActive(true);
         JumpCam.SetActive(false);
-        FlashIng.SetActive(false);
+        //FlashIng.SetActive(false);
 
     }
 }
---
+++ Assets/Til huset/Hessburg - Triangle Set Square/Scripts/DemoRotations.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoRotations : MonoBehaviour 
+{
+
+	public Transform SunLight;
+	public Transform CamParent;
+	public Transform Triangle1;
+	public Transform Triangle2;
+	public Transform Triangle3;
+
+	void Update () 
+	{
+		SunLight.Rotate(new Vector3(20.0f* Time.deltaTime, 15.0f* Time.deltaTime, 0.0f), Space.World);
+		Triangle1.Rotate(new Vector3(53.0f* Time.deltaTime, 50.0f* Time.deltaTime, 0.0f), Space.World);
+		Triangle2.Rotate(new Vector3(50.0f* Time.deltaTime, 50.0f* Time.deltaTime, 0.0f), Space.World);
+		Triangle3.Rotate(new Vector3(47.0f* Time.deltaTime, 50.0f* Time.deltaTime, 0.0f), Space.World);
+		CamParent.Rotate(new Vector3(0.0f, -45.0f* Time.deltaTime, 0.0f), Space.World);
+	}
+}
